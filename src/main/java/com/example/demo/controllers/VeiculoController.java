653d4a6d0e75f3a84ee22ceeb0ba80765ed5b0f5
giovannibrito <giovannileonibrito@bol.com.br>
2023-05-07 16:28:18 -0300

    Adiciona exercicio 4

--- src/main/java/com/example/demo/controllers/VeiculoController.java
+++ src/main/java/com/example/demo/controllers/VeiculoController.java
@@ -1,2 +1,44 @@
-package com.example.demo.controllers;public class VeiculoController {
+package com.example.demo.controllers;
+
+import com.example.demo.dtos.VeiculoDto;
+import com.example.demo.models.Veiculo;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/veiculos")
+@RestController
+public class VeiculoController {
+    private VeiculoService service;
+
+    public VeiculoController(VeiculoService service) {
+        this.service = service;
+    }
+
+    @PostMapping()
+    public Veiculo cadastrarVeiculo(@Valid @RequestBody VeiculoDto request){
+        return service.cadastrar(request);
+    }
+
+    @GetMapping()
+    public List<Veiculo> buscarTodosVeiculos(){
+        return service.buscarTodos();
+    }
+
+    @GetMapping("/{placa}")
+    public Veiculo buscarVeiculoPelaPlaca(@PathVariable String placa){
+        return service.buscarVeiculoPelaPlaca(placa);
+    }
+
+    @DeleteMapping("/{placa}")
+    public ResponseEntity<String> excluirVeiculo(@PathVariable String placa){
+        return service.excluirVeiculo(placa);
+    }
+
+    @PutMapping("/{placa}/multas")
+    public Veiculo adicionarMulta(@PathVariable String placa){
+        return service.cadastrarMulta(placa);
+    }
 }
--- src/main/java/com/example/demo/dtos/VeiculoDto.java
+++ src/main/java/com/example/demo/dtos/VeiculoDto.java
@@ -1,2 +1,16 @@
-package com.example.demo.dtos;public class VeiculoDto {
+package com.example.demo.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class VeiculoDto {
+    @NotEmpty(message = "O campo placa é obrigatório")
+    private String placa;
+    @NotEmpty(message = "O campo tipo é obrigatório")
+    private String tipo;
+    @NotEmpty(message = "O campo cor é obrigatório")
+    private String cor;
+    @NotEmpty(message = "O campo anoDeFabricacao é obrigatório")
+    private Integer anoDeFabricacao;
 }
