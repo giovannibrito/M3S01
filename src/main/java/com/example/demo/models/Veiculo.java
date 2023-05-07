993418d725d0b6ba1c28ae0049093308454836df
giovannibrito <giovannileonibrito@bol.com.br>
2023-05-06 23:39:30 -0300

    Adiciona exercicio 2

---
+++ src/main/java/com/example/demo/models/Veiculo.java
@@ -0,0 +1,16 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Veiculo {
+    @Id
+    private String placa;
+    private String tipo;
+    private String cor;
+    private Integer anoDeFabricacao;
+    private Integer qtdMultas;
+}
